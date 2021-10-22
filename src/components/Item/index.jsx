7141d5beadfc3379df7a1d176233509c62573561
SWQXDBA <983110853@qq.com>
2021-10-23 00:54:07 +0800

    2

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,12 +2,22 @@ import React, {Component} from 'react';
 
 class Item extends Component {
 
-
+state = {
+    finish:false
+}
+constructor(props) {
+    super(props)
+    this.state.finish = props.finish
+}
+change = ()=>{
+    const {finish} = this.state
+    this.setState({finish:!finish})
+}
     render() {
         return (
             <li onMouseOver={this.showDelete}>
                 <label >
-                    <input type="checkbox" checked={this.props.finish} onChange={()=>{}}/>
+                    <input type="checkbox" checked={this.state.finish} onChange={this.change}/>
                     <span>{this.props.text}</span>
                 </label>
                 <button ref={'delete'} style={{display:'none'}}>删除</button>
